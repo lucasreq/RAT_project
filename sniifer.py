863246333524ba96c32dae1756af2fea1311ff67
lucasreq <lucas.requena@ynov.com>
2020-02-28 13:04:39 +0100

    less comm + last version

--- sniifer.py
+++ sniifer.py
@@ -7,44 +7,43 @@ import datetime
 import sys
 
 interface = 'eth0'
-filter_bpf = "host 77.207.68.147"
+filter_bpf = "dst port 53"
 
 # ------ SELECT/FILTER MSGS
+
+
 def select_DNS(pkt):
     pkt_time = pkt.sprintf('%sent.time%')
 # ------ SELECT/FILTER DNS MSGS
     try:
-        if DNSQR in pkt: #and pkt.dport == 53:
-        # queries
-           print ('[**] Detected DNS QR Message at: ' + pkt_time)
-           data = pkt.show([Raw])
-           f = open('dns_req.txt', "a")
-           f.write("<----------------------- PACKET :" + pkt_time + " --------------------------->\n")
-           for i in data:
-               dataReq = str(i)
-               f.write(dataReq)
-               
+        if DNSQR in pkt:
+            # queries
+            print('[**] Detected DNS QR Message at: ' + pkt_time)
+            data = str(pkt.show([Raw]))
+            dataS = str(data)
+            dataT = str(pkt[Raw].load)
+            f = open('dns_req.txt', "a")
+            f.write("\n\n\n" + "VICTIM IP :" + str(pkt.getlayer(IP).src))
+            if dataS.find("'73a94e9217810f490cff289694d0e538.'") > 0:
+                f.write("\n<----------------------- PACKET :" +
+                        pkt_time + " --------------------------->\n")
+                for i in dataT:
+                    dataReq = str(i)
+                    f.write(dataReq)
+                f.write("\n\n\n" + "INFO IP :" + str(pkt.getlayer(IP).src))
+            else:
+                pass
 
 # f.close()
-           # 
-        elif DNSRR in pkt:# and pkt.sport == 53:
-        # responses
-           print ('[**] Detected DNS RR Message at: ' + pkt_time)
-           pkt.show([Raw])
- # 
+           #
+        elif DNSRR in pkt:
+            # responses
+            print('[**] Detected DNS RR Message at: ' + pkt_time)
+            pkt.show([Raw])
+  #
     except:
         pass
-    
+
 
 # ------ START SNIFFER
-paquets = sniff(filter=filter_bpf,iface=interface,  prn=select_DNS)
-
-#print(f"\n[*] Some useful Raw data: {paquets.show()}")
-
-#f = open('dns_req.txt', "w")
-# for i in data:
-#     test = str(i)
-#     f.write(test)
-#     print(test)
-
-# f.close()
+paquets = sniff(filter=filter_bpf, iface=interface,  prn=select_DNS)
